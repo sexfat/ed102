2348a05f4d662720e0cfbb3203056bedef8d5c3a
sexfat <bryant6831@gmail.com>
2020-06-16 14:00:58 +0800

    add scroll

--- scroll.js
+++ scroll.js
@@ -1,3 +1,12 @@
-TweenMax.to('.m_2' , 1 , {
-    x: 200
-}) 
+var controller  = new ScrollMagic.Controller();
+
+ var m2 = TweenMax.to('.m_2' , 1 , {
+    x: 200,
+    backgroundColor: '#f20'
+});
+
+
+new ScrollMagic.Scene({
+   triggerElement: "#trigger"
+}).setTween(m2).addIndicators().addTo(controller);
+
